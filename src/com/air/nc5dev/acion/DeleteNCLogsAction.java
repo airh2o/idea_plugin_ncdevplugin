80fc4f7223a6bb053a824bedd343f78b839e165c
air <airh2o02@qq.com>
2020-02-21 15:20:06 +0800

    update-NC日志清理支持U8Cloud

--- src/com/air/nc5dev/acion/DeleteNCLogsAction.java
+++ src/com/air/nc5dev/acion/DeleteNCLogsAction.java
@@ -15,13 +15,24 @@ public class DeleteNCLogsAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         try {
-            if(StringUtil.isEmpty(ProjectNCConfigUtil.getNCHomePath())){
+            if (StringUtil.isEmpty(ProjectNCConfigUtil.getNCHomePath())) {
                 ProjectUtil.warnNotification("未配置NC HOME", e.getProject());
-                return ;
+                return;
             }
-            File dir = new File(ProjectNCConfigUtil.getNCHomePath() + File.separatorChar + "nclogs" );
+            File dir = new File(ProjectNCConfigUtil.getNCHomePath() + File.separatorChar + "nclogs");
+
+            if (!dir.exists()) {
+                dir = new File(ProjectNCConfigUtil.getNCHomePath() + File.separatorChar + "u8clogs");
+            }
+
+            if (!dir.exists()) {
+                ProjectUtil.warnNotification("日志文件夹路径不存在，请使用NC标准文件夹名: " + dir.getPath(), e.getProject());
+            }
 
             IoUtil.deleteFileAll(dir);
+            dir.mkdir();
+
+            ProjectUtil.infoNotification("日志文件清理完成: " + dir.getPath(), e.getProject());
         } catch (Exception iae) {
             ProjectUtil.errorNotification(ExceptionUtil.getExcptionDetall(iae), e.getProject());
         }
