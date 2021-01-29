ae053aebfa1438b68df9cae826ee927a798c6ddd
tangsuqq@126.com <ts123456>
2021-01-29 19:33:10 +0800

    add-增加定时提醒

--- src/com/air/nc5dev/acion/SetingRemindAction.java
+++ src/com/air/nc5dev/acion/SetingRemindAction.java
@@ -2,12 +2,13 @@ package com.air.nc5dev.acion;
 
 import com.air.nc5dev.acion.base.AbstractIdeaAction;
 import com.air.nc5dev.ui.PatcherDialog;
+import com.air.nc5dev.ui.SetingRemindDialog;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
 public class SetingRemindAction extends AbstractIdeaAction {
     @Override
     protected void doHandler(AnActionEvent e) {
-        PatcherDialog dialog = new PatcherDialog(e);
+        SetingRemindDialog dialog = new SetingRemindDialog(e);
         if(dialog.showAndGet()){
             dialog.onOK();
         }
