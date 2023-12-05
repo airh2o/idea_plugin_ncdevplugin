a2a43eba08964ab5be0d3006be1f9fb5c6ab7931
tangsuqq@126.com <tangsuqq@126.com>
2023-12-05 11:43:53 +0800

    update-生成数据字典优化界面和逻辑并且增加对U8C和NC5的支持

--- front/element-demo/src/components/DataDictionary.vue
+++ front/element-demo/src/components/DataDictionary.vue
@@ -516,21 +516,12 @@ export default {
     handleTabsEdit(targetName, action) {
       console.log('handleTabsEdit...', targetName, action)
 
-      if (action === 'add') {
-        let newTabName = ++this.tabIndex + '';
-        this.editableTabs.push({
-          title: 'New Tab',
-          name: newTabName,
-          content: 'New Tab content'
-        });
-        this.editableTabsValue = newTabName;
-      }
       if (action === 'remove') {
-        let tabs = this.editableTabs;
-        let activeName = this.editableTabsValue;
-        if (activeName === targetName) {
+        let tabs = this.tabsList;
+        let activeName = this.showTabsValue;
+        if (activeName == targetName) {
           tabs.forEach((tab, index) => {
-            if (tab.name === targetName) {
+            if (tab.name == targetName) {
               let nextTab = tabs[index + 1] || tabs[index - 1];
               if (nextTab) {
                 activeName = nextTab.name;
@@ -539,8 +530,8 @@ export default {
           });
         }
 
-        this.editableTabsValue = activeName;
-        this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+        this.showTabsValue = activeName;
+        this.tabsList = tabs.filter(tab => tab.name != targetName);
       }
     },
     buildTableClassTypeName(item) {
